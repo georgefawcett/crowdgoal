dbab7a3bb63978f775e0d9d53bb52aeca4d2ef5f
George Fawcett <georgealanfawcett@gmail.com>
2017-04-26 15:07:08 +0000

    s3 correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,7 @@ config.paperclip_defaults = {
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
   }
 }
 
