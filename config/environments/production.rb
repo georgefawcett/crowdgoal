b6c82637224f36f8860f52ab853855565c2c6af2
George Fawcett <georgealanfawcett@gmail.com>
2017-04-26 14:24:43 +0000

    Heroku s3 correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,15 +81,16 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-Paperclip.options[:command_path] = "/usr/local/bin/"
+
 
 config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
 }
 
   # Do not dump schema after migrations.
