25ef67b56621be80395ba282cd910fb332df3e7b
AbhiMuthukumar <abhi.muthukumar@gmail.com>
2017-05-01 14:55:56 +0000

    edit user is in progress. commit before the merge monday morning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,8 +39,8 @@ function onLoad(){
   $('.dropdown-button').dropdown({
       inDuration: 300,
       outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
+      constrainWidth: true, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
       gutter: 0, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
       alignment: 'left', // Displays dropdown with edge aligned to the left of button
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # @user.password = user_params["password"]
-    # if password_matches? then
       if (@user.save)
         session[:user_id] = @user.id
         redirect_to '/events'
       else
         render :new
       end
-    # else
-    #   @user.errors.add(:password, "Password did not match!")
-    #   render :new
-    # end
+  end
+
+  def edit
+
   end
 
   def show
@@ -28,22 +26,12 @@ class UsersController < ApplicationController
 
     @joined = "SELECT count(*) FROM events_users
                      WHERE  user_id = #{@user.id}"
-
-
-
-
     following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = #{@user.id}"
     @following = User.where("id IN (#{following_ids})")
-
     follower_ids = "SELECT follower_id FROM relationships
                      WHERE  followed_id = #{@user.id}"
     @followers = User.where("id IN (#{follower_ids})")
-
-
-
-
-
   end
 
 
