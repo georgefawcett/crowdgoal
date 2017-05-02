f4f9c5b7cdb84a3764ad4f545a5d5ef06500c76e
AbhiMuthukumar <abhi.muthukumar@gmail.com>
2017-05-02 17:20:51 +0000

    done with final minor changes of navbar and other stuffs

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,10 @@ class EventsController < ApplicationController
 
   now = Time.now.beginning_of_day
   @events = Event.joins("INNER JOIN events_users
-ON events_users.event_id = events.id
-where events.start_date > '#{now}'
-group by events.id
-having count(events_users.event_id) < events.max_people")
+    ON events_users.event_id = events.id
+    where events.start_date > '#{now}'
+    group by events.id
+    having count(events_users.event_id) < events.max_people")
   # @events = Event.where('start_date > :end',
   #   :end=> now.beginning_of_day,
   # ).order('start_date', 'start_time')
