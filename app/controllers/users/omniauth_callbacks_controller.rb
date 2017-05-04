d8c6b6126125dca8935a26836d735bf9690918c2
George Fawcett <georgealanfawcett@gmail.com>
2017-05-04 18:16:05 +0000

    Heroku

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def
-    @event = Event.find(params[:id])
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if !session[:user_id]
       session[:user_id] = User.find_by(email: @user.email).id
     end
 
-    puts @event
 
     if @user.persisted?
 
-      if params[:id]
 
-        redirect_to events_path, :id => params[:id]
-      else
         redirect_to events_path
-      end
+
       # sign_in_and_redirect @user, :event => :authentication
       # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
 
-      if params[:id]
-        redirect_to events_path, :id => params[:id]
-      else
+
         redirect_to events_path
-      end
+
       # redirect_to 'events#index'
     end
   end
