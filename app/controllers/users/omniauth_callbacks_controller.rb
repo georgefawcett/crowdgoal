2cfc28d80f76aad3701d7dfbaaef49d1c50bcc43
George Fawcett <georgealanfawcett@gmail.com>
2017-05-04 18:20:03 +0000

    FB

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,24 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def
-
+  def facebook
+    @event = Event.find(params[:id])
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if !session[:user_id]
       session[:user_id] = User.find_by(email: @user.email).id
     end
-
-
+    puts "The event is #{@event}"
     if @user.persisted?
-
-
         redirect_to events_path
 
       # sign_in_and_redirect @user, :event => :authentication
       # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
 
-
         redirect_to events_path
-
       # redirect_to 'events#index'
     end
   end
@@ -27,3 +22,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path
   end
 end
+
